ba9c4810efc6cefd7ae0a60448546fbf3186f3a5
amirbarari <amirmohammadbarari415@gmail.com>
2025-07-28 19:21:22 +0330

    styling book card component

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -5,7 +5,7 @@ import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
 import { Sidebar } from "../sidebar/sidebar";
 import { Books } from "./books/books";
-import { CommonModule } from "@angular/common";
+import { CommonModule, CurrencyPipe } from "@angular/common";
 import { BookCard } from "./books/book-card/book-card";
 import { DashboardService } from "./services/dashboard/dashboard.service";
 import { routes } from "./dashboard.routing.module";
@@ -19,7 +19,7 @@ import { CreateBook } from "./create-book/create-book";
         RouterOutlet, Header,
         Footer, Sidebar,
         CommonModule, Button,
-        ReactiveFormsModule,],
+        ReactiveFormsModule,CurrencyPipe],
     providers: [DashboardService]
 })
 export class DashboardModule { }
